cfee0265bcaa5beffa779fdf92f1b8f6339c13e8
ManjuBashini <103051896+kiddoGirl@users.noreply.github.com>
2023-12-03 11:04:30 +0530

    commit

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
