a0887342e24428bedcd1fd4b8ac7512cd517aad6
BhuvanaLakshmi <bhuvanalakshmi003@gmail.com>
2024-02-08 14:32:24 +0530

    Leaderboard and Trade page

--- assets/js/leaderboard.js
+++ assets/js/leaderboard.js
@@ -6,71 +6,71 @@ const team = [
 		name: 'Lewis Hamilton',
 		handle: 'lewishamilton',
 		img: 'https://www.formula1.com/content/dam/fom-website/drivers/L/LEWHAM01_Lewis_Hamilton/lewham01.png.transform/2col-retina/image.png',
-		kudos: 36,
-		sent: 31
+		Brought: 36,
+		Sold: 31
 	}, {
 		rank: 2,
 		name: 'Kimi Raikkonen',
 		handle: 'kimimatiasraikkonen',
 		img: 'https://www.formula1.com/content/dam/fom-website/drivers/K/KIMRAI01_Kimi_R%C3%A4ikk%C3%B6nen/kimrai01.png.transform/2col-retina/image.png',
-		kudos: 31,
-		sent: 21
+		Brought: 31,
+		Sold: 21
 	}, {
 		rank: 3,
 		name: 'Sebastian Vettel',
 		handle: 'vettelofficial',
 		img: 'https://www.formula1.com/content/dam/fom-website/drivers/S/SEBVET01_Sebastian_Vettel/sebvet01.png.transform/2col-retina/image.png',
-		kudos: 24,
-		sent: 7
+		Brought: 24,
+		Sold: 7
 	}, {
 		rank: 4,
 		name: 'Max Verstappen',
 		handle: 'maxverstappen1',
 		img: 'https://www.formula1.com/content/dam/fom-website/drivers/M/MAXVER01_Max_Verstappen/maxver01.png.transform/2col-retina/image.png',
-		kudos: 22,
-		sent: 4
+		Brought: 22,
+		Sold: 4
 	}, {
 		rank: 5,
 		name: 'Lando Norris',
 		handle: 'landonorris',
 		img: 'https://www.formula1.com/content/dam/fom-website/drivers/L/LANNOR01_Lando_Norris/lannor01.png.transform/2col-retina/image.png',
-		kudos: 18,
-		sent: 16
+		Brought: 18,
+		Sold: 16
 	}, {
 		rank: 6,
 		name: 'Charles Leclerc',
 		handle: 'charles_leclerc',
 		img: 'https://www.formula1.com/content/dam/fom-website/drivers/C/CHALEC01_Charles_Leclerc/chalec01.png.transform/2col-retina/image.png',
-		kudos: 16,
-		sent: 6
+		Brought: 16,
+		Sold: 6
 	}, {
 		rank: 7,
 		name: 'George Russell',
 		handle: 'georgerussell63',
 		img: 'https://www.formula1.com/content/dam/fom-website/drivers/G/GEORUS01_George_Russell/georus01.png.transform/2col-retina/image.png',
-		kudos: 10,
-		sent: 21
+		Brought: 10,
+		Sold: 21
 	}, {
 		rank: 8,
 		name: 'Daniel Ricciardo',
 		handle: 'danielricciardo',
 		img: 'https://www.formula1.com/content/dam/fom-website/drivers/D/DANRIC01_Daniel_Ricciardo/danric01.png.transform/2col-retina/image.png',
-		kudos: 7,
-		sent: 46
+		Brought: 7,
+		Sold: 46
 	}, {
 		rank: 9,
 		name: 'Alexander Albon',
 		handle: 'alex_albon',
 		img: 'https://www.formula1.com/content/dam/fom-website/drivers/A/ALEALB01_Alexander_Albon/alealb01.png.transform/2col-retina/image.png',
-		kudos: 4,
-		sent: 2
+		Brought: 4,
+		Sold: 2
 	}, {
 		rank: 10,
 		name: 'Carlos Sainz Jr.',
 		handle: 'carlossainz55',
 		img: 'https://www.formula1.com/content/dam/fom-website/drivers/C/CARSAI01_Carlos_Sainz/carsai01.png.transform/2col-retina/image.png',
-		kudos: 1,
-		sent: 24
+		Brought: 1,
+		Sold: 24
 	}
 ];
 
@@ -95,7 +95,12 @@ team.forEach(member => {
 			</div>
 			<div class="u-text--right c-kudos">
 				<div class="u-mt--8">
-					<strong>${member.kudos}</strong> ${randomEmoji()}
+					<strong>${member.Brought}</strong> ${randomEmoji()}
+				</div>
+			</div>
+			<div class="u-text--right c-kudos">
+				<div class="u-mt--8">
+					<strong>${member.Sold}</strong> ${randomEmoji()}
 				</div>
 			</div>
 		</div>
